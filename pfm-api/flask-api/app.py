a495512b571f284929c620cf3b819eda6cfd2537
Osasona Ifeoluwa <osasonaifeoluwa1103@gmail.com>
2023-02-16 00:40:03 +0100

    bug fix

--- pfm-api/flask-api/app.py
+++ pfm-api/flask-api/app.py
@@ -154,7 +154,7 @@ def process():
     
     # get data
     query = request.json
-    account_name = query['account_name']
+    account_name = query['account_name'].lower()
 
     df = pd.DataFrame(query['transactions'])
     data = df.copy()
